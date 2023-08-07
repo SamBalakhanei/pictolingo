afed56f19b51ef76641aa96379b9f8a2cc6666bc
Sam <thesamloop@gmail.com>
2023-08-07 00:13:37 -0400

    switch db to nonlocal

--- app/api/MongooseActions.jsx
+++ app/api/MongooseActions.jsx
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 import dotenv from 'dotenv'
 dotenv.config();
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.ATLAS_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
