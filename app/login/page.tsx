b0b7402cc067b8448df70673660242706678e56d
SamBalakhanei <thesamloop@gmail.com>
2024-08-06 17:42:19 -0400

    edit signin/out page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,17 @@
-import { GoogleSignInButton } from "../components/authButtons"
-import Navbar from "../components/Navbar"
+import { GoogleSignInButton } from "../components/authButtons";
+import Navbar from "../components/Navbar";
 
 export default function page() {
   return (
     <>
-    <Navbar />
-        <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
-            <div className="flex flex-col items-center w-1/3 mt-10 p-10 shadow-lg bg-slate-200">
-                <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
-                <GoogleSignInButton />
-            </div>
-
+      <Navbar />
+      <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col items-center w-full max-w-md mt-10 p-8 shadow-lg rounded-lg bg-white">
+          <h1 className="mt-10 mb-4 text-4xl text-gray-800 font-bold">Sign In</h1>
+          <p className="mb-8 text-gray-600 text-center">Sign in with your Google account to get started</p>
+          <GoogleSignInButton />
         </div>
-        
+      </div>
     </>
-  )
+  );
 }
--- app/signout/page.tsx
+++ app/signout/page.tsx
@@ -1,18 +1,16 @@
-import { SignOutButton } from "../components/authButtons"
-import Navbar from "../components/Navbar"
+import { SignOutButton } from "../components/authButtons";
+import Navbar from "../components/Navbar";
 
 export default function page() {
   return (
     <>
-    <Navbar />
-        <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
-            <div className="flex flex-col items-center mt-10 p-10 shadow-lg bg-slate-200">
-                <h1 className="mt-10 mb-4 text-4xl font-bold">Are you sure you want to sign out?</h1>
-                <SignOutButton />
-            </div>
-
+      <Navbar />
+      <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col items-center w-full max-w-md mt-10 p-8 shadow-lg rounded-lg bg-white">
+          <h1 className="mt-10 mb-4 text-4xl text-gray-800 font-bold text-center">Are you sure you want to sign out?</h1>
+          <SignOutButton />
         </div>
-        
+      </div>
     </>
-  )
+  );
 }
